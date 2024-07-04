45be6987ea5051ff5ac0683ce6c5064fe7f17f84
illysito <illyalukyanovdesign@gmail.com>
2024-07-04 15:05:50 +0100

    Update hero.js

--- src/features/hero.js
+++ src/features/hero.js
@@ -3,47 +3,50 @@ import { gsap } from 'gsap'
 function hero_type() {
   let swap_duration = 0
   let swap_delay = 1.2
+  let mm = gsap.matchMedia()
 
-  gsap.to('.soy-illy-wrapper', {
-    opacity: 1,
-    duration: swap_duration,
-    repeat: -1,
-    repeatDelay: 3 * swap_delay,
-  })
-  gsap.to('.soy-illy-wrapper', {
-    opacity: 0,
-    delay: 1 * swap_delay,
-    duration: swap_duration,
-    repeat: -1,
-    repeatDelay: 3 * swap_delay,
-  })
-  gsap.to('.soy-illy-wrapper-en', {
-    opacity: 1,
-    delay: 1 * swap_delay,
-    duration: swap_duration,
-    repeat: -1,
-    repeatDelay: 3 * swap_delay,
-  })
-  gsap.to('.soy-illy-wrapper-en', {
-    opacity: 0,
-    delay: 2 * swap_delay,
-    duration: swap_duration,
-    repeat: -1,
-    repeatDelay: 3 * swap_delay,
-  })
-  gsap.to('.soy-illy-wrapper-arab', {
-    opacity: 1,
-    delay: 2 * swap_delay,
-    duration: swap_duration,
-    repeat: -1,
-    repeatDelay: 3 * swap_delay,
-  })
-  gsap.to('.soy-illy-wrapper-arab', {
-    opacity: 0,
-    delay: 3 * swap_delay,
-    duration: swap_duration,
-    repeat: -1,
-    repeatDelay: 3 * swap_delay,
+  mm.add('(min-width:478px)', () => {
+    gsap.to('.soy-illy-wrapper', {
+      opacity: 1,
+      duration: swap_duration,
+      repeat: -1,
+      repeatDelay: 3 * swap_delay,
+    })
+    gsap.to('.soy-illy-wrapper', {
+      opacity: 0,
+      delay: 1 * swap_delay,
+      duration: swap_duration,
+      repeat: -1,
+      repeatDelay: 3 * swap_delay,
+    })
+    gsap.to('.soy-illy-wrapper-en', {
+      opacity: 1,
+      delay: 1 * swap_delay,
+      duration: swap_duration,
+      repeat: -1,
+      repeatDelay: 3 * swap_delay,
+    })
+    gsap.to('.soy-illy-wrapper-en', {
+      opacity: 0,
+      delay: 2 * swap_delay,
+      duration: swap_duration,
+      repeat: -1,
+      repeatDelay: 3 * swap_delay,
+    })
+    gsap.to('.soy-illy-wrapper-arab', {
+      opacity: 1,
+      delay: 2 * swap_delay,
+      duration: swap_duration,
+      repeat: -1,
+      repeatDelay: 3 * swap_delay,
+    })
+    gsap.to('.soy-illy-wrapper-arab', {
+      opacity: 0,
+      delay: 3 * swap_delay,
+      duration: swap_duration,
+      repeat: -1,
+      repeatDelay: 3 * swap_delay,
+    })
   })
 }
 
