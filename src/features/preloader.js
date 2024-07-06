2177dfbe2641cc0cf8d9953575bb569784073a60
illysito <illyalukyanovdesign@gmail.com>
2024-07-06 15:15:08 +0100

    oisdhg

--- src/features/preloader.js
+++ src/features/preloader.js
@@ -3,29 +3,21 @@ function preloader_count() {
   let currentValue = 0
 
   function updateCounter() {
-    function isMobile() {
-      return window.innerWidth <= 478 // or use any threshold you consider as mobile
+    document.querySelector('.preloader-counter-wrapper').style.visibility =
+      'none'
+    if (currentValue === 100) {
+      localStorage.setItem('preloaderShown', 'true')
+      return
     }
-
-    if (isMobile()) {
-      console.log('mobile')
-    } else {
-      document.querySelector('.preloader-counter-wrapper').style.visibility =
-        'none'
-      if (currentValue === 100) {
-        localStorage.setItem('preloaderShown', 'true')
-        return
-      }
-      console.log('we in')
-      currentValue += Math.floor(Math.random() * 10)
-      if (currentValue > 100) {
-        currentValue = 100
-      }
-      counterElement.textContent = currentValue
-
-      let delay = Math.floor(Math.random() * 200) + 50
-      setTimeout(updateCounter, delay)
+    console.log('we in')
+    currentValue += Math.floor(Math.random() * 10)
+    if (currentValue > 100) {
+      currentValue = 100
     }
+    counterElement.textContent = currentValue
+
+    let delay = Math.floor(Math.random() * 200) + 50
+    setTimeout(updateCounter, delay)
   }
   updateCounter()
 }
--- src/features/preloader_animation.js
+++ src/features/preloader_animation.js
@@ -7,26 +7,24 @@ function preloader_animation() {
 
   if (isMobile()) {
     console.log('mobile')
-    // let complete = false
-    // gsap.to('.preloader-counter-wrapper', {
-    //   opacity: 0,
-    //   delay: 3.5,
-    //   duration: 1,
-    // })
-    // gsap.to('.preloader-overlay-mobile', {
-    //   delay: 3.5,
-    //   opacity: 0,
-    //   ease: 'power4.inOut',
-    //   onComplete: () => {
-    //     complete = true
-    //   },
-    // })
-    // if (complete) {
-    //   document.querySelector('.preloader-counter-wrapper').style.visibility =
-    //     'hidden'
-    //   document.querySelector('.preloader-overlay-mobile').style.visibility =
-    //     'hidden'
-    // }
+    let complete = false
+    gsap.to('.preloader-counter-wrapper', {
+      opacity: 0,
+      delay: 3.5,
+      duration: 1,
+    })
+    gsap.to('.preloader-overlay-mobile', {
+      delay: 3.5,
+      opacity: 0,
+      ease: 'power4.inOut',
+      onComplete: () => {
+        complete = true
+      },
+    })
+    if (complete) {
+      document.querySelector('.preloader-counter-wrapper').style.zIndez = 0
+      document.querySelector('.preloader-overlay-mobile').style.zIndex = 0
+    }
   } else {
     gsap.to('.preloader-counter-wrapper', {
       opacity: 0,
