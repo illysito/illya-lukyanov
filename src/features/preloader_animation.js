8c465b427f8ecfea5e3d88946b6b020cb7256e7b
illysito <illyalukyanovdesign@gmail.com>
2024-07-06 15:04:41 +0100

    Update preloader_animation.js

--- src/features/preloader_animation.js
+++ src/features/preloader_animation.js
@@ -6,26 +6,27 @@ function preloader_animation() {
   }
 
   if (isMobile()) {
-    let complete = false
-    gsap.to('.preloader-counter-wrapper', {
-      opacity: 0,
-      delay: 3.5,
-      duration: 1,
-    })
-    gsap.to('.preloader-overlay-mobile', {
-      delay: 3.5,
-      opacity: 0,
-      ease: 'power4.inOut',
-      onComplete: () => {
-        complete = true
-      },
-    })
-    if (complete) {
-      document.querySelector('.preloader-counter-wrapper').style.visibility =
-        'hidden'
-      document.querySelector('.preloader-overlay-mobile').style.visibility =
-        'hidden'
-    }
+    console.log('mobile')
+    // let complete = false
+    // gsap.to('.preloader-counter-wrapper', {
+    //   opacity: 0,
+    //   delay: 3.5,
+    //   duration: 1,
+    // })
+    // gsap.to('.preloader-overlay-mobile', {
+    //   delay: 3.5,
+    //   opacity: 0,
+    //   ease: 'power4.inOut',
+    //   onComplete: () => {
+    //     complete = true
+    //   },
+    // })
+    // if (complete) {
+    //   document.querySelector('.preloader-counter-wrapper').style.visibility =
+    //     'hidden'
+    //   document.querySelector('.preloader-overlay-mobile').style.visibility =
+    //     'hidden'
+    // }
   } else {
     gsap.to('.preloader-counter-wrapper', {
       opacity: 0,
