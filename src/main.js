e0d4671009ba7785923b38e419a3dc2b71a0a0c8
illysito <illyalukyanovdesign@gmail.com>
2025-10-19 00:50:18 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -21,49 +21,63 @@ gsap.registerPlugin(ScrollTrigger)
 
 // ------------------------------------------------------------ HERO TYPE!
 
-hero_type()
-function init() {
-  // Check if the preloader has been shown before
-  const preloaderShown = localStorage.getItem('preloaderShown')
+function runHomeFunctions() {
+  hero_type()
+  function init() {
+    // Check if the preloader has been shown before
+    const preloaderShown = localStorage.getItem('preloaderShown')
 
-  if (!preloaderShown) {
-    // Show the preloader if it hasn't been shown before
-    document.querySelector('.preloader-counter-wrapper').style.display = 'flex'
-    document.querySelector('.preloader-overlay-desktop').style.display = 'flex'
-    document.querySelector('.preloader-overlay-mobile').style.display = 'flex'
-    preloader_count()
-    preloader_animation()
-  } else {
-    // Hide the preloader if it has been shown before
-    document.querySelector('.preloader-counter-wrapper').style.display = 'none'
-    document.querySelector('.preloader-overlay-desktop').style.display = 'none'
-    document.querySelector('.preloader-overlay-mobile').style.display = 'none'
+    if (!preloaderShown) {
+      // Show the preloader if it hasn't been shown before
+      document.querySelector('.preloader-counter-wrapper').style.display =
+        'flex'
+      document.querySelector('.preloader-overlay-desktop').style.display =
+        'flex'
+      document.querySelector('.preloader-overlay-mobile').style.display = 'flex'
+      preloader_count()
+      preloader_animation()
+    } else {
+      // Hide the preloader if it has been shown before
+      document.querySelector('.preloader-counter-wrapper').style.display =
+        'none'
+      document.querySelector('.preloader-overlay-desktop').style.display =
+        'none'
+      document.querySelector('.preloader-overlay-mobile').style.display = 'none'
+    }
   }
+  init()
+
+  // ------------------------------------------------------------ PORTFOLIO LIFE IMAGES
+
+  swap_posters()
+  portfolio_life()
+
+  // ------------------------------------------------------------ PORTFOLIO PARALLAX //
+
+  portfolio_hero_parallax()
+
+  // ------------------------------------------------------------ PORTFOLIO ACCORDION //
+
+  accordion()
+  hover_lines()
+
+  // ------------------------------------------------------------ MENU TYPE //
+
+  swap_type()
+
+  // ------------------------------------------------------------ PACKAGING PARALLAX //
+
+  packaging_parallax()
+
+  // ------------------------------------------------------------ SPLT TYPE //
+
+  split_type()
 }
-init()
 
-// ------------------------------------------------------------ PORTFOLIO LIFE IMAGES
+function runBriefingFunctions() {
+  console.log('hey')
+  split_type()
+}
 
-swap_posters()
-portfolio_life()
-
-// ------------------------------------------------------------ PORTFOLIO PARALLAX //
-
-portfolio_hero_parallax()
-
-// ------------------------------------------------------------ PORTFOLIO ACCORDION //
-
-accordion()
-hover_lines()
-
-// ------------------------------------------------------------ MENU TYPE //
-
-swap_type()
-
-// ------------------------------------------------------------ PACKAGING PARALLAX //
-
-packaging_parallax()
-
-// ------------------------------------------------------------ SPLT TYPE //
-
-split_type()
+if (document.body.classList.contains('body__home')) runHomeFunctions()
+if (document.body.classList.contains('body__briefing')) runBriefingFunctions()
